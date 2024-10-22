307e844cb4c633305f469791d59fc8bd1bbfaee9
Aziz <imazizul@gmail.com>
2024-10-22 10:56:58 +0600

    Added base activity

---
+++ app/src/main/java/com/tsl/baseapp/ui/base/BaseActivity.kt
@@ -0,0 +1,59 @@
+package com.tsl.baseapp.ui.base
+
+import android.app.Dialog
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<VM : BaseViewModel, VB : ViewBinding> : AppCompatActivity() {
+    abstract val mViewModel: VM
+
+    protected lateinit var mViewBinding: VB
+    lateinit var dialog: Dialog
+    private val logoutHandler = Handler(Looper.getMainLooper())
+    abstract fun getViewBinding(): VB
+    abstract fun setLanguageTexts()
+    abstract fun setActionBarTitle(title: String?)
+    abstract fun setActionBar()
+    abstract fun hideActionBar()
+    abstract fun onBackPressListener()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mViewBinding = getViewBinding()
+        dialog = Dialog(this)
+        setUpObservers()
+    }
+
+    private fun setUpObservers() {
+        mViewModel.showLoader.observe(this) { it ->
+            it.getContentIfNotHandled()?.let { isShow ->
+                //manageLoader(isShow)
+            }
+
+        }
+
+        mViewModel.showMessage.observe(this) { message ->
+            message.getContentIfNotHandled()?.let {
+                if (!it.isNullOrEmpty()) {
+                    //showCustomDialog(message = it)
+                }
+            }
+        }
+
+
+        mViewModel.isLanguageUpdateNeeded.observe(this) { isUpdateNeeded ->
+            if (isUpdateNeeded) {
+                setLanguageTexts()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (dialog.isShowing) dialog.dismiss()
+    }
+
+}
---
+++ app/src/main/java/com/tsl/baseapp/ui/base/BaseFragment.kt
@@ -0,0 +1,75 @@
+package com.tsl.baseapp.ui.base
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VM : BaseViewModel, VB : ViewBinding> : Fragment() {
+    protected abstract val mViewModel: VM
+
+    protected lateinit var mViewBinding: VB
+    private lateinit var dialog: Dialog
+
+    abstract fun getViewBinding(): VB
+    abstract fun setLanguageTexts()
+    private lateinit var fm: FragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        dialog = Dialog(requireContext())
+        fm = requireActivity().supportFragmentManager
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpObservers()
+    }
+
+
+    private fun setUpObservers() {
+
+        mViewModel.showLoader.observe(viewLifecycleOwner) { it ->
+            it.getContentIfNotHandled()?.let { isShow ->
+                val activity = requireActivity()
+                if (activity is BaseActivity<*, *>) {
+                    //activity.manageLoader(isShow)
+                }
+            }
+        }
+
+        mViewModel.showMessage.observe(viewLifecycleOwner) { message ->
+            message.getContentIfNotHandled()?.let {
+                if (!it.isNullOrEmpty()) {
+                    //showCustomDialog(message = it)
+                }
+            }
+        }
+    }
+
+
+
+
+    override fun onPause() {
+        super.onPause()
+        val activity = requireActivity()
+        if (activity is BaseActivity<*, *>) {
+            //activity.cancelInactivityTimer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val activity = requireActivity()
+        if (activity is BaseActivity<*, *>) {
+            //activity.addMoneyBottomSheetDialog?.dismiss()
+        }
+        //  hideKeyboard(requireContext(), requireView())
+        setLanguageTexts()
+    }
+
+
+}
